f53f08ec5b9d3d1b6117fa53ec6f26fda4860eb5
wickedside <w1ck3ds1d3@gmail.com>
2023-05-23 21:35:13 +0300

    Changed function VotingAction

--- app/src/main/java/com/topic3/android/reddit/components/Post.kt
+++ app/src/main/java/com/topic3/android/reddit/components/Post.kt
@@ -199,6 +199,19 @@ fun VotingAction(
   onDownVoteAction: () -> Unit
 ) {
   //TODO add your code here
+  Row(verticalAlignment = Alignment.CenterVertically){
+    ArrowButton(
+      onUpVoteAction,
+      R.drawable.ic_baseline_arrow_upward_24
+    )
+    Text(
+      text = text,
+      color = Color.Gray,
+      fontWeight = FontWeight.Medium,
+      fontSize = 12.sp
+    )
+    ArrowButton(onDownVoteAction, R.drawable.ic_baseline_arrow_downward_24)
+  }
 }
 
 @Composable
